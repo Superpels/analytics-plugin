07ccde5bc1b5a7f05aa0c5ae627d76cecc975bab
Pels Sauco <antonio@sauco-web.com>
2017-06-09 13:56:09 +0200

    Add files via upload

---
+++ form-file.php
@@ -0,0 +1,10 @@
+<div class="wrap">
+	<h1>Google Analytics</h1>
+	<h3>Plugin desarrollado por <a href="https://sauco-web.com" target="_blank">Saúco Web</a></h3>
+	<p>El código de Google Analytics solo se inserta cuando no estamos <em>logueados</em></p>
+	<form method="POST">
+	    <label for="awesome_text">Introduce aquí tu código de Analytics</label>
+	    <input type="text" name="awesome_text" id="awesome_text" value="<?php echo $value; ?>">
+	    <input type="submit" value="Guardar" class="button button-primary">
+	</form>
+</div>
