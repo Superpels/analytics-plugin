71adac9ed02d3c9780ba50491c9bb40547187e91
Pels Sauco <antonio@sauco-web.com>
2017-06-09 13:54:28 +0200

    Create google-analytics.php

--- google-analytics.php
+++ google-analytics.php
@@ -1,9 +1,9 @@
 <?php
 /**
-* Plugin Name:	Google Analytics
+* Plugin Name:	Google Analytics por Saúco Web
 * Plugin URI: 	http://sauco-web.com/
-* Description: 	Para activar Google Analytics. Simplemente deja el plugin activo y Google Analytics recibirá datos
-* Version:	1.0
+* Description: 	Para activar Google Analytics. Entra en el menú Ajustes -> Google Analytics e introduce tu código
+* Version:	1.1
 * Author: 	Antonio García-Saúco Iglesias
 * Author URI: 	http://sauco-web.com/
 */
@@ -15,23 +15,54 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if this file is accessed directly
 
 // ==============================================
-//  The plugin
+//  Creando el menú de la página
 // ==============================================
 
-add_action( 'wp_head', 'sauco_web_ga_script' );
-function sauco_web_ga_script() {
-	
-	if ( !is_user_logged_in() ) { //Solo coge datos si el usuario NO está logueado
-	?>
-	<script type="text/javascript">
+add_action('admin_menu', 'analytics_page');
+function analytics_page() {
+    
+    $parent_slug	= 'options-general.php';
+    $page_title 	= 'Google Analytics';
+    $menu_title 	= 'Google Analytics';
+    $capability 	= 'edit_posts';
+    $menu_slug 		= 'sauco_analytics';
+    $function		= 'analytics_page_display';
+    
+    add_submenu_page( $parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function); 
+}
+
+// ==============================================
+//  Construyendo la página en el backend
+// ==============================================
+
+function analytics_page_display() {
+    if (isset($_POST['awesome_text'])) {
+        update_option('awesome_text', $_POST['awesome_text']);
+        $value = $_POST['awesome_text'];
+    } 
+
+    $value = get_option('awesome_text', 'UA-XXXXXXXX-X');
+    if (is_admin()){
+    	include 'form-file.php';
+    }
+
+// ==============================================
+//  Insertando el código de Analytics en la web
+// ==============================================
+    
+    if ( !is_user_logged_in() ) { //Solo coge datos si el usuario NO está logueado
+	?>		
+	<script>
 	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 	
-	ga('create', 'UA-XXXXXXXX-X', 'auto'); //Cambiar UA-XXXXXXXX-X por tu código de Google
-	ga('send', 'pageview');
+	  ga('create', '<?php echo $value;?>', 'auto');
+	  ga('send', 'pageview');
+	
 	</script>
 	<?php
 	}
 }
+add_action( 'wp_head', 'analytics_page_display' );
